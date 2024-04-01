f3103a4dc8b85af156edebc407f8922e5d82e646
IvanBel <ivbel84021UlIM@yandex.ru>
2024-04-01 17:25:31 +0300

    imports from java to your imports

--- src/test/java/ru/mirea/TrppTest.java
+++ src/test/java/ru/mirea/TrppTest.java
@@ -1,11 +1,13 @@
 package ru.mirea;
 
-import io.micronaut.runtime.EmbeddedApplication;
-import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import javax.inject.Inject;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
-import javax.inject.Inject;
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+
 
 @MicronautTest
 class TrppTest {
