9a37fa8ae1e91ae24f851c9398f8262a93487f5b
IvanBel <ivbel84021UlIM@yandex.ru>
2024-04-01 17:28:07 +0300

    imports3

--- src/test/java/ru/mirea/TrppTest.java
+++ src/test/java/ru/mirea/TrppTest.java
@@ -1,8 +1,8 @@
 package ru.mirea;
 
 import javax.inject.Inject;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 
